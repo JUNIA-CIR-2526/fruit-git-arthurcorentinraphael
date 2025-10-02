9d37becf5ced22f25f867e6fd6b57d51b0cf0d3e
ArthurPch <arthur.pocholle@gmail.com>
2025-10-02 09:51:38 +0200

    tenez les frr je suis gv chaud

---
+++ src/main/java/com/jad/Fruit.java
@@ -0,0 +1,15 @@
+package com.jad;
+
+public class Fruit {
+    public String getName() {
+        return name;
+    }
+
+    private final String name;
+
+    public Fruit(String name) {
+        this.name = name;
+    }
+
+
+}
